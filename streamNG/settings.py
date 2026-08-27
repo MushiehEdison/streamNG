2c3f48803fdc82783df61f86d3bf0b1c478ac11d
Mushieh Edison <mushiehedison66@gmail.com>
2026-08-27 04:05:02 +0100

    stream

--- streamNG/settings.py
+++ streamNG/settings.py
@@ -145,9 +145,6 @@ ASGI_APPLICATION = 'streamNG.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
